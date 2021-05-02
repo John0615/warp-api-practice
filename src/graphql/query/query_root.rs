00d2db910e7fc1bf92f7e3e6623b7fbe6d5099fe
John <john.zhao@scrumcn.com>
2021-05-02 10:32:44 +0800

    增加测试字段

--- src/graphql/query/query_root.rs
+++ src/graphql/query/query_root.rs
@@ -4,6 +4,7 @@ struct Book {
   id: i32,
   title: String,
   author: String,
+  describe: String,
 }
 
 #[Object]
@@ -18,6 +19,10 @@ impl Book {
   async fn author(&self) -> &str {
       &self.author
   }
+
+  async fn describe(&self) -> &str {
+      &self.describe
+  }
 }
 
 pub struct QueryRoot;
@@ -30,16 +35,19 @@ impl QueryRoot {
               id: 1,
               title: "踢啊四四四口大口大口大口".to_string(),
               author: "John".to_string(),
+              describe: "这是一本好书".to_string()
           },
           Book {
               id: 2,
               title: "上课送我我哦饿哦饿".to_string(),
               author: "Jobin".to_string(),
+              describe: "这是一本好书".to_string()
           },
           Book {
               id: 3,
               title: "i为i为i开".to_string(),
               author: "Edward".to_string(),
+              describe: "这是一本好书".to_string()
           },
       ]
   }
