f54294b791f05b275a117b20f10bcaca2da933fb
John <john.zhao@scrumcn.com>
2021-05-13 09:05:11 +0800

    优化新增用户

--- src/service/user_service.rs
+++ src/service/user_service.rs
@@ -38,21 +38,31 @@ pub async fn get_user_phone_number(
   let phone_number_wrapper = my_pool.new_wrapper().eq("phone_number", phone_number);
   let user = my_pool.fetch_by_wrapper::<LgUser>("", &phone_number_wrapper).await;
 
+  println!("user>>>>>> {:?}", user);
+
   if user.is_ok() {
       Ok(user.unwrap())
   } else {
-      Err(Error::new("email 不存在")
+      Err(Error::new("phone_number 不存在")
           .extend_with(|_, e| e.set("details", "1_EMAIL_NOT_EXIStS")))
   }
 }
 
-pub async fn insert_user(my_pool: &Rbatis, new_lg_user: NewLgUser) -> GqlResult<LgUser> {
-  let save_result = my_pool.save("", &new_lg_user).await;
-  match save_result {
-    Ok(value) => {
-      println!("has value {:?}", value);
-      self::get_user_phone_number(my_pool, &"17722443746".to_string()).await
-    },
-    Err(_error) => Err(Error::new("find-user-error").extend_with(|_, e| e.set("details", "No records"))),
+pub async fn insert_user(my_pool: &Rbatis, mut new_lg_user: NewLgUser) -> GqlResult<LgUser> {
+  // email 转小写
+  new_lg_user.email1 = new_lg_user.email1.to_lowercase();
+  if self::get_user_phone_number(my_pool, &new_lg_user.phone_number).await.is_ok() {
+    Err(Error::new("phone_number 已存在")
+        .extend_with(|_, e| e.set("details", "1_EMAIL_EXIStS")))
+  } else {
+    let save_result = my_pool.save("", &new_lg_user).await;
+    match save_result {
+      Ok(_value) => {
+        println!("phone_number {:?}", &new_lg_user.phone_number);
+        self::get_user_phone_number(my_pool, &new_lg_user.phone_number).await
+      },
+      Err(_error) => Err(Error::new("save-user-error").extend_with(|_, e| e.set("save", "error"))),
+    }
   }
+  
 }
